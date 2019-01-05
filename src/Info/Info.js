d29e1eb2fb075f1001df6800c94c32f8ea9e1d45
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-05 14:37:55 -0700

    Create Info component and info test files for use in aside

---
+++ src/Info/Info.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Info(props) {
+    return (
+      <div>
+        {!props.quizStarted && 
+          <div>
+            <p>Instructions</p>
+            <ol>
+              <li>Click StartQuiz</li>
+              <li>Choose the image that may be produced if the named flexbox property were in your CSS</li>
+              <li>If you get stuck, check back here to get more information about the flexbox property</li>
+              <li>Enjoy creating simple layouts with flexbox!</li>
+            </ol>
+          </div>
+        }
+        {props.quizStarted && !props.showInfo && 
+            <p onClick={props.toggleShowInfo}>
+              Click Here to show more info about: 
+              <span>{props.currentQuestion.property}</span> 
+            </p>    
+        }
+        {props.quizStarted && props.showInfo && 
+            <p onClick={props.toggleShowInfo}>
+              {props.currentQuestion.info}
+            </p>
+        }
+      </div>
+    )
+}
+
+export default Info;
