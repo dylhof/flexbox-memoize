3502b623ed93b7a83f94b77b32d3ecc7f48d7242
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-05 14:32:58 -0700

    Create  aside component with aside test file

---
+++ src/Aside/Aside.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Info from '../Info/Info.js';
+
+function Aside(props) {
+  return (
+    <aside>
+      <div> 
+        <h3>Your Score:</h3>
+        <p>{props.correctAnswers} Correct Answers</p>
+        <p>{props.answeredQuestions} Questions Answered</p>
+      </div>
+      <div>
+        <button className='learn-more-btn' >
+          <a href='https://css-tricks.com/snippets/css/a-guide-to-flexbox/' target='_blank'>
+            Learn more about flexBox!
+          </a>
+        </button>
+        <Info 
+          currentQuestion={props.currentQuestion}
+          showInfo={props.showInfo}
+          quizStarted={props.quizStarted}
+          toggleShowInfo={props.toggleShowInfo}/>
+      </div>
+    </aside>
+  )
+}
+
+export default Aside;
