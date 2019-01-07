bd40a7bec02f73330e657401ee7fdb8ecd909770
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-06 19:54:49 -0700

    Update the styles for questions and question card

--- src/Question/Question.js
+++ src/Question/Question.js
@@ -3,14 +3,16 @@ import React from 'react';
 function Question(props){
     return(
       <div className='question-div'>
-        <h2>{props.currentQuestion.property}</h2>
-        <p>If you included this property in your CSS, 
-          what image most closely resembles the result?</p>
-        <div>
+        <div className='question-title-div'>
+          <h2>{props.currentQuestion.property}</h2>
+          <p>If you included this property in your CSS, 
+            what image most closely resembles the result?</p>
+        </div>
+        <div className='question-img-div'>
           {props.randomAnswers.map(answer => {
             return (
               <img
-                className="answer--img"
+                className='answer-img'
                 src={answer}
                 alt={answer}
                 key={answer}
