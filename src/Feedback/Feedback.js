280a46bfe00c4e05a58a626453bc061ec608d9b2
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-06 20:12:29 -0700

    add correct image to feedback

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 function Feedback(props) {
   return(  
-    <div>
+    <div className='feedback-div'>
       <h3> {props.message} </h3>
-      <p> {props.currentQuestion.property} </p>
-      <p> {props.currentQuestion.info} </p>
+      <p className='feedback-property-p'> {props.currentQuestion.property} </p>
+      <p className='feedback-info-p'> {props.currentQuestion.info} </p>
+      <img 
+      className='answer-img'
+      src={props.currentQuestion.correctAnswer}
+      alt={props.currentQuestion.property}
+      key={props.currentQuestion.property}
+      onClick={props.checkAnswer} />
       <button onClick={props.resetAnswerGiven}> Next Question </button>
     </div>
     )
